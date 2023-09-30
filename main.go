6ba54adf657765dd7e9617b9cc5d2a8e942fd7cd
Alex Svetkin <alex.svetkin@gmail.com>
2023-10-01 01:53:42 +0200

    WIP

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gocolly/colly"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/thedevsaddam/gojsonq/v2"
@@ -18,6 +19,7 @@ type QuestionStat struct {
 type Question struct {
 	Difficulty map[string]int `json:"difficulty"`
 	Stat       QuestionStat   `json:"stat"`
+	PaidOnly   bool           `json:"paid_only"`
 }
 
 func main() {
@@ -33,5 +35,29 @@ func main() {
 	var questions []Question
 	gojsonq.New().Reader(resp.Body).From("stat_status_pairs").Out(&questions)
 
-	fmt.Println(questions)
+	fmt.Println(len(questions))
+
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"),
+	)
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("X-Requested-With", "XMLHttpRequest")
+		r.Headers.Set("Content-Type", "application/json;charset=UTF-8")
+		fmt.Println("Visiting: ", r.URL)
+	})
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println(".")
+		fmt.Println(string(r.Body))
+	})
+	c.OnError(func(r *colly.Response, e error) {
+		fmt.Println("error:", e, r.Request.URL, string(r.Body))
+	})
+	for _, q := range questions {
+		if !q.PaidOnly {
+			jsonStr := `{"query":"\n    query questionContent($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    content\n    mysqlSchemas\n    dataSchemas\n  }\n}\n    ","variables":{"titleSlug":"%s"},"operationName":"questionContent"}`
+			jsonStr = fmt.Sprintf(jsonStr, q.Stat.TitleSlug)
+			c.PostRaw("https://leetcode.com/graphql", []byte(jsonStr))
+		}
+	}
+	c.Wait()
 }
